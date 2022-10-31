8d82a173b977546b620ecae7b7e6c3132ba4c178
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-10-31 13:52:04 +0700

    peta base adt

--- src/ADT/headers/peta.h
+++ src/ADT/headers/peta.h
@@ -12,39 +12,25 @@
 #define SPACE '#'
 /* Nilai elemen objek kosong */
 #define SIMULATOR 'S'
-#define TELEPHONE 'T'
-#define MIXER 'M'
-#define CHOPPER 'C'
-#define FRYER 'F'
-#define BOILER 'B'
-#define WALL 'X'
+// #define TELEPHONE 'T'
+// #define MIXER 'M'
+// #define CHOPPER 'C'
+// #define FRYER 'F'
+// #define BOILER 'B'
+// #define WALL 'X'
 
 /* Definisi elemen dan koleksi objek */
-typedef char LegType;
-typedef struct {
-    LegType Type;
-    POINT Lokasi;
-} Legend;
+typedef char Legend;
 typedef struct {
     int widthEff; // Lebar peta (Vertikal)
     int lengthEff; // Panjang peta (Horizontal)
-    Legend* Map[WIDTH_CAP][LENGTH_CAP]; // Pointer to Legend
-    Legend contents[WIDTH_CAP*LENGTH_CAP]; // Collection of Legends
+    Legend Map[WIDTH_CAP][LENGTH_CAP]; // Pointer to Legend
 } Peta;
-
-/* *** Notasi Akses: Selektor Legend *** */
-#define TYPE(L) (L).Type
-#define LOC(L) (L).Lokasi
-#define ABSIS(L) (L).Lokasi.X
-#define ORDINAT(L) (L).Lokasi.Y
 
 /* *** Notasi Akses: Selektor Peta *** */
 #define LEN_EFF(P) (P).lengthEff
 #define WIDTH_EFF(P) (P).widthEff
 #define ELMT(P, x, y) (P).Map[y][x]
-#define MAP_POINT(P, x, y) (P).Map[y][x]->Lokasi
-#define MAP_TYPE(P, x, y) (P).Map[y][x]->Type
-#define LEGENDS(P) (P).contents
 
 /* *** DEFINISI PROTOTIPE PRIMITIF *** */
 /* *** Konstruktor membentuk Peta *** */
@@ -60,28 +46,37 @@ void LoadPeta(Peta *P, Word map);
 /* F.S. Elemen-elemen P terdefinisi sesuai Word map */
 
 /* *** Selektor Peta *** */
-boolean isLocationValid(int x, int y);
-/* Mengirimkan true jika <x,y> adalah titik yang valid dalam sistem Peta */
+boolean isLocationOutOfBound(Peta P, POINT p);
+/* Mengirimkan true jika <x,y> adalah titik di dalam sistem Peta */
+
+boolean isLocationColliding(Peta P, POINT p);
+/* Mengirimkan true jika <x,y> tidak mengalami tubrukan dengan titik lain */
+
+boolean isLocationEff(Peta P, POINT p);
+/* Mengirimkan true jika <x,y> adalah titik yang efektif dalam sistem Peta */
 
 /* *** Selektor: Untuk sebuah peta P yang terdefinisi: *** */
-Legend getLegendOf(Peta *P, LegType t);
-/* Mendapatkan Legend dari Legend Type t dalam sistem Peta */
-
-POINT locationOf(Peta *P, LegType t);
+POINT locationOf(Peta *P, Legend t);
 /* Mendapatkan lokasi <x,y> dari Legend Type t dalam sistem Peta */
 
 /* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */                           
-void MoveLegend(Legend *L, Peta *P, int x, int y);
-/* Menggeser Legend dalam sistem Peta sejauh <x,y> */
+void MoveLegend(POINT LegPoint, Peta *P, char direction);
+/* Menggeser Legend dalam sistem Peta sejauh satu satuan ke arah direction */
+/* direction: NORTH, EAST, WEST, SOUTH */
 /* I.S. L dan P terdefinisi */
-/* F.S. L dalam sistem Peta bergeser sejauh <x,y> atau
-        mencetak prompt jika <x,y> menyebabkan Legend melebihi batas */
+/* F.S. L dalam sistem Peta bergeser sejauh satu ke direction atau
+        mencetak prompt jika direction menyebabkan Legend melebihi batas atau menabrak dinding */
 
 void MoveSimulator(Peta *P, char direction);
 /* Menggeser Simulator sesuai arah mata angin sejauh satu satuan dalam sistem Peta */
 /* I.S. Legend Simulator terdefinisi dalam P */
 /* F.S. Simulator bergerak sesuai definisi atau
-        mencetak prompt jika direction tidak valid */
+        mencetak prompt jika direction menyebabkan Legend melebihi batas atau menabrak dinding */
 
+/* ********** KELOMPOK BACA/TULIS ********** */
+void DisplayPeta(Peta P);
+/* I.S. P terdefinisi */
+/* F.S. Nilai P.Map[i][j] ditampilkan ke layar per baris per kolom
+        Peta dikelilingi oleh simbol '*' sebagai pembatas*/
 
 #endif
--- src/ADT/implementation/peta.c
+++ src/ADT/implementation/peta.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include ".\ADT\headers\peta.h"
+#include "../headers/peta.h"
 
 /* *** DEFINISI PROTOTIPE PRIMITIF *** */
 void CreatePeta(int length, int width, Peta *P){
@@ -9,24 +9,115 @@ void CreatePeta(int length, int width, Peta *P){
     int x, y;
     for(y = 0; y<=width; y++){
         for(x = 0; x<=length; x++){
-            ELMT(*P, x, y) = NULL;
+            ELMT(*P, x, y) = SPACE;
         }
     }
 }
 
 void LoadPeta(Peta *P, Word map){
-    
+    int current = 0;
+    int i = 0;
+    int j = 0;
+
+    while(current < map.Length){
+        if(map.TabWord[current] == '\n'){
+            j = 0;
+            i++;
+        }else{ // current char != '\n'
+            ELMT(*P, j, i) = map.TabWord[current];
+        }
+        current++;
+        j++;
+    }
 }
 
 /* *** Selektor Peta *** */
-boolean isLocationValid(int x, int y);
+boolean isLocationOutOfBound(Peta P, POINT p){
+    return Kuadran(p)!=1  || (int)Absis(p)>=LEN_EFF(P) || (int)Ordinat(p)>=WIDTH_EFF(P);
+}
+
+boolean isLocationColliding(Peta P, POINT p){
+    return ELMT(P, (int)Absis(p), (int)Ordinat(p)) != SPACE;
+}
+
+boolean isLocationEff(Peta P, POINT p){
+    return isLocationOutOfBound(P, p) || isLocationColliding(P, p);
+}
 
 /* *** Selektor: Untuk sebuah peta P yang terdefinisi: *** */
-Legend getLegendOf(Peta *P, LegType t);
+POINT locationOf(Peta *P, Legend t){
+    int x, y;
+    POINT p;
 
-POINT locationOf(Peta *P, LegType t);
+    for(y = 0; y<WIDTH_EFF(*P); y++){
+        for(x = 0; x<LEN_EFF(*P); x++){
+            if(ELMT(*P, x, y) == t){
+                CreatePoint(&p, x, y);
+                return p;
+            }
+        }
+    }
+}
 
 /* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */                           
-void MoveLegend(Legend *L, Peta *P, int x, int y);
+void MoveLegend(POINT LegPoint, Peta *P, char direction){
+    int x, y, nx, ny;
+    x = (int) Absis(LegPoint);
+    y = (int) Ordinat(LegPoint);
 
-void MoveSimulator(Peta *P, char direction);
+    switch(direction){
+        case 'N':
+            Geser(&LegPoint, 0, 1);
+            break;
+        case 'S':
+            Geser(&LegPoint, 0, -1);
+            break;
+        case 'W':
+            Geser(&LegPoint, -1, 0);
+            break;
+        case 'E':
+            Geser(&LegPoint, 1, 0);
+            break;
+    }
+
+    if(isLocationOutOfBound(*P, LegPoint)){
+        printf("Out of bound");
+    }
+    else if(isLocationColliding(*P, LegPoint)){
+        printf("Collision");
+    }
+    else{
+        nx = (int) Absis(LegPoint);
+        ny = (int) Ordinat(LegPoint);
+
+        ELMT(*P, nx, ny) = ELMT(*P, x, y);
+        ELMT(*P, x, y) = SPACE;
+    }
+}
+
+void MoveSimulator(Peta *P, char direction){
+    POINT LegPoint = locationOf(P, SIMULATOR);
+    MoveLegend(LegPoint, P, direction);
+}
+
+void DisplayPeta(Peta P){
+    int x, y;
+    int i;
+
+    for(i = 0; i < LEN_EFF(P)+2; i++){ // Mencetak * di awal Peta
+        printf("*");
+    }
+    printf("\n");
+    for(y = 0; y < WIDTH_EFF(P); y++){
+        printf("*");
+        for(x = 0; x < LEN_EFF(P); x++){
+            if(ELMT(P, x, y) == SPACE) printf(" ");
+            else printf("%c", ELMT(P, x, y));
+        }
+        printf("*\n");
+    }
+    for(i = 0; i < LEN_EFF(P)+2; i++){ // Mencetak * di akhir Peta
+        printf("*");
+    }
+}
+
