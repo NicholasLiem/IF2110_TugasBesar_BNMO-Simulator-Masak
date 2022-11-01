ead16aa8e842a58fd63d997b297c7a979023c4be
williamnixon20 <75229742+williamnixon20@users.noreply.github.com>
2022-11-01 07:42:49 +0700

    [ADD] command.h

---
+++ src/Helper/headers/command.h
@@ -0,0 +1,27 @@
+#ifndef _COMMAND_h
+#define _COMMAND_h
+
+#include <stdio.h>
+#include "../../ADT/headers/wordmachine.h"
+#include "../../ADT/headers/listlinier.h"
+#include "../../ADT/headers/tree.h"
+#include "../../ADT/headers/makanan.h"
+#include "../../ADT/headers/word.h"
+#include "../../ADT/headers/peta.h"
+#include "simulator.h"
+
+
+void printMenu();
+
+void notInput();
+
+void promptName(Word *word);
+
+void menuHasNotLogin();
+
+void menuHasLogin();
+
+void displayInfo();
+
+void splashArt();
+#endif
