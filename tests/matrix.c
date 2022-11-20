1c697e116d2141e0a74dcf88fc78b5a229a0f0e1
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-11-20 09:53:25 +0700

    feat: matrix driver

--- tests/matrix.c
+++ tests/matrix.c
@@ -1,9 +1,30 @@
 #include "../src/adt/matrix/matrix.h"
+#include <stdio.h>
 
-// gcc -std=c17 -I. src/adt/charmachine/*.c src/adt/kulkas/*.c src/adt/listlinier/*.c src/adt/liststatik/*.c src/adt/makanan/*.c src/adt/matrix/*.c src/adt/peta/*.c src/adt/point/*.c src/adt/prioqueuedinamik/*.c src/adt/set/*.c src/adt/stack/*.c src/adt/time/*.c src/adt/tree/*.c src/adt/word/*.c src/adt/wordmachine/*.c src/helper/command/*.c src/helper/configparser/*.c src/helper/simulator/*.c -o bin/matrix tests/matrix.c
+// gcc -std=c17 -I. src/adt/matrix/*.c -o bin/matrix tests/matrix.c
 int main() {
     Matrix p;
-    createMatrix(3, 3, &p);
-    readMatrix(&p, 3, 3);
+    int a, b;
+    printf("Ukuran matriks: ");
+    scanf("%d%d", &a, &b);
+    createMatrix(a, b, &p);
+    readMatrix(&p, a, b);
+
+    printf("A:\n");
     displayMatrix(p);
+    printf("Banyak elemen: %d\n", countElmt(p));
+
+    if(isSquare(p)){
+        printf("Matriks persegi\n");
+        Matrix ps = multiplyMatrix(p, p);
+        printf("A^2:\n");
+        displayMatrix(ps);    
+    }
+    else printf("Bukan matriks persegi\n");
+
+    printf("Determinan: %f\n", determinant(p));
+
+    printf("AT:\n");
+    displayMatrix(transpose(p));
+
 }
